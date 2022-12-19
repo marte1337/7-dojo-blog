ecf480cbd72000a7f60aa07c2e7d077f61f8e270
marte1337 <54943747+marte1337@users.noreply.github.com>
2022-12-19 18:09:52 +0100

    db setup

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,9 +1,34 @@
 <template>
-    <div class="post-list"></div>
+    <div class="post-list">
+        <div v-for="post in posts" :key="post.id">
+            <!-- props erneut weiterleiten, Zugriff auf "post" durch iterate oben -->
+            <SinglePost :post="post" />
+        </div>
+    </div>
 </template>
 
 
 
 <script>
+import { onMounted, onUpdated, onUnmounted } from "vue";
+import SinglePost from "./SinglePost.vue"
+export default {
 
+    //"posts" abholen
+    props: ['posts'], 
+
+    components: { SinglePost },
+
+    //um props im setup zu nutzen, als Argument angeben
+    setup(props) {
+        //lifecycle-hooks im setup ebenfalls möglich, heißen dann statt mounted() -> onMounted() / updated() -> onUpdated() / etc.
+        onMounted(() => console.log("component mounted"))
+        onUpdated(() => console.log("component updated"))
+        onUnmounted(() => console.log("component unmounted"))
+    }
+
+}
 </script>
+
+<style>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,53 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+
+      <!-- props erstellen :posts="posts" -->
+      <PostList :posts="posts" />
+
+    </div>
+    <div v-else>Loading...</div>
+
   </div>
 </template>
 
 
 
 <script>
+import PostList from "../components/PostList.vue";
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
+  components: { PostList },
   setup() {
-    const posts = ref([
-      { title: 'welcome to the blog', body: 'Lorem ipsum', id: 1 },
-      { title: 'top 5 CSS tips', body: 'Lorem ipsum', id: 2 }
-    ])
 
+    //posts wird in der "load"-Funktion befüllt: posts.value = await data.json()
+    const posts = ref([ ])
+    const error = ref(null)
 
+    //async & await
+    const load = async () => {
+      try {
+        let data = await fetch("http://localhost:3000/posts")
+        console.log(data)
+        if (!data.ok) {
+          throw Error("no data available")
+        }
+        posts.value = await data.json()
+      }
+      catch (err) {
+        error.value = err.message
+        console.log(error.value)
+      }
 
-    return { posts }
+    }
+
+    load()
+
+    return { posts, error }
   }
 }
 </script>
