e75239f43a01d51560a56bebe4519fadcedd1c06
marte1337 <54943747+marte1337@users.noreply.github.com>
2022-12-19 14:11:03 +0100

    build up

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="home">
-
+    <h1>Home</h1>
   </div>
 </template>
+
+
 
 <script>
 
 export default {
   name: 'HomeView',
-  components: { }
+  setup() {
+    const posts = ref([
+      { title: 'welcome to the blog', body: 'Lorem ipsum', id: 1 },
+      { title: 'top 5 CSS tips', body: 'Lorem ipsum', id: 2 }
+    ])
+
+
+
+    return { posts }
+  }
 }
 </script>
